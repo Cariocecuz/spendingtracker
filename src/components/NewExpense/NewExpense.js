fdc35ff01f6a78525a1e973e8f3fca1b7ac1a43f
Cariocecuz <fvelosa@gmail.com>
2021-12-02 18:14:28 +0000

    "finished" expense tracker

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,37 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 function NewExpense(props) {
+  const [newExpenseClicked, setNewExpenseClicked] = useState(false);
+
   function saveExpenseDataHandler(enteredExpenseData) {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setNewExpenseClicked(false);
   }
 
+  function onClickHandle() {
+    setNewExpenseClicked(true);
+  }
+
+  function stopEditing(props) {
+    setNewExpenseClicked(false);
+  }
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!newExpenseClicked && (
+        <button onClick={onClickHandle}>Add new expense</button>
+      )}
+      {newExpenseClicked && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditing}
+        />
+      )}
     </div>
   );
 }
